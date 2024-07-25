52b694fe8f4789f980450b5bb0c7946555baef47
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-07-25 16:51:45 +0530

    Fix possible perms issue

--- restore.sh
+++ restore.sh
@@ -1,13 +1,13 @@
 # avoid logging secrets!
 set -e
 
-echo '+ echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" > serviceAccountKey.json'
-echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" > serviceAccountKey.json
+echo '+ echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" > /tmp/serviceAccountKey.json'
+echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" > /tmp/serviceAccountKey.json
 
 # safe to turn on logging now
 set -ex
 
-gcloud auth activate-service-account --key-file serviceAccountKey.json
+gcloud auth activate-service-account --key-file /tmp/serviceAccountKey.json
 
 # restore the database
 
