708b66d7398c98df036de5d46ec4083df2f5aa86
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-07-25 17:25:21 +0530

    Try new restore every 24 hours

--- restore.Dockerfile
+++ restore.Dockerfile
@@ -7,8 +7,8 @@ RUN apt-get update && apt-get install -y curl python3
 WORKDIR /tmp
 
 COPY ./restore.sh ./restore.sh
+COPY ./run-restore.sh ./run-restore.sh
 
-RUN chmod 0777 ./restore.sh
 RUN mkdir -p $PGDATA
 RUN chown -R postgres:postgres $PGDATA
 
@@ -20,4 +20,4 @@ RUN curl -s https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz \
     | tar xz && ./google-cloud-sdk/install.sh -q --usage-reporting=false
 ENV PATH=$PATH:google-cloud-sdk/bin/
 
-CMD ["./restore.sh"]
+CMD ["./run-restore.sh"]
--- restore.sh
+++ restore.sh
@@ -1,26 +1,13 @@
-# avoid logging secrets!
-set -e
+pg_isready && pg_ctl -D $PGDATA stop && echo "Stopped running database..."
 
-echo '+ echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" > /tmp/serviceAccountKey.json'
-echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" > /tmp/serviceAccountKey.json
-
-# safe to turn on logging now
-set -ex
-
-gcloud auth activate-service-account --key-file /tmp/serviceAccountKey.json
-
-# restore the database
-
+echo "Deleting existing DB and restoring from $BUCKET_PATH"
+rm -rf $PGDATA/*
 gsutil cp $BUCKET_PATH - | tar -I zstd -xvf - -C $PGDATA
 pg_ctl -D $PGDATA start
 
-while true;
-do
-  if pg_isready -h localhost -p 5432;
-  then
-    echo "Database is ready"
-  else
-    echo "Database is not ready"
-  fi
-  sleep 24h
-done
+if pg_isready -h localhost -p 5432;
+then
+  echo "Database is ready"
+else
+  echo "Database is not ready"
+fi
