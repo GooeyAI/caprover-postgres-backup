93401f24ec725fc963cff301b3c66a573ab68730
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-07-25 19:08:11 +0530

    fix perms on $PGDATA after restore

--- restore.sh
+++ restore.sh
@@ -3,6 +3,9 @@ pg_isready && pg_ctl -D $PGDATA stop && echo "Stopped running database..."
 echo "Deleting existing DB and restoring from $BUCKET_PATH"
 rm -rf $PGDATA/*
 gsutil cp $BUCKET_PATH - | tar -I zstd -xvf - -C $PGDATA
+
+chmod 0700 $PGDATA
+
 pg_ctl -D $PGDATA start
 
 if pg_isready -h localhost -p 5432;
